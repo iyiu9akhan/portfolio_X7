eab0f390b9be6434e26b0c6881fdfe549c9091fa
iyiu9akhan <iyiu9akhan@gmail.com>
2025-06-12 22:46:32 +0600

    windows: formating

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,31 @@ import ErrorPage from "./components/layout/errorPage/ErrorPage";
 import Banner from "./components/banner/Banner";
 import Expertise from "./components/expertise/Expertise";
 // import ShinyText from '../reactBiteAnimation/ShinyText/ShinyText'
-// import { useEffect } from "react";
+import { useEffect } from "react";
 
 function App() {
-  // const [count, setCount] = useState(0);
-  // useEffect(() => {
-  //   const handleContextMenu = (e) => e.preventDefault();
-  //   const handleKeyDown = (e) => {
-  //     if (
-  //       e.key === "F12" ||
-  //       (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "i") ||
-  //       (e.ctrlKey && e.key.toLowerCase() === "u")
-  //     ) {
-  //       e.preventDefault();
-  //     }
-  //   };
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    const handleContextMenu = (e) => e.preventDefault();
+    const handleKeyDown = (e) => {
+      if (
+        e.key === "F12" ||
+        (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "i") ||
+        (e.ctrlKey && e.key.toLowerCase() === "u")
+      ) {
+        e.preventDefault();
+      }
+    };
 
-  //   document.addEventListener("contextmenu", handleContextMenu);
-  //   document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("contextmenu", handleContextMenu);
+    document.addEventListener("keydown", handleKeyDown);
 
-  //   // Cleanup on unmount
-  //   return () => {
-  //     document.removeEventListener("contextmenu", handleContextMenu);
-  //     document.removeEventListener("keydown", handleKeyDown);
-  //   };
-  // }, []);
+    // Cleanup on unmount
+    return () => {
+      document.removeEventListener("contextmenu", handleContextMenu);
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
 
   return (
     <>
